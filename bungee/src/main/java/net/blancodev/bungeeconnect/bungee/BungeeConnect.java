77b8035eb9d754cce34bc35ffa542c554225918f
Joey <b3tanyan@gmail.com>
2022-06-21 19:55:24 -0400

    Create bungee module

---
+++ bungee/src/main/java/net/blancodev/bungeeconnect/bungee/BungeeConnect.java
@@ -0,0 +1,13 @@
+package net.blancodev.bungeeconnect.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeConnect extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+        
+
+    }
+}
