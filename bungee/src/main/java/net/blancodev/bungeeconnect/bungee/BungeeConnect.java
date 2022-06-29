213fecbba5e06211ce3ff721c1ed6e6f34f0f9e7
Joey <b3tanyan@gmail.com>
2022-06-29 03:54:24 -0400

    Test bungee adding servers

--- bungee/src/main/java/net/blancodev/bungeeconnect/bungee/BungeeConnect.java
+++ bungee/src/main/java/net/blancodev/bungeeconnect/bungee/BungeeConnect.java
@@ -2,12 +2,15 @@ package net.blancodev.bungeeconnect.bungee;
 
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.net.InetSocketAddress;
+
 public class BungeeConnect extends Plugin {
 
     @Override
     public void onEnable() {
-
-
-
+        getProxy().getServers().put("tester", getProxy().constructServerInfo(
+            "tester", InetSocketAddress.createUnresolved("127.0.0.1", 25568), "motd", false
+        ));
     }
+
 }
